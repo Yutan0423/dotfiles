91a09619cf889703832400096cc0aed3bdb5e392
Yutan0423 <yuutan3240@gmail.com>
2024-01-10 20:41:34 +0900

    a

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,12 +2,23 @@ return {
   {
     "tiagovla/tokyodark.nvim",
     opts = {
-        -- custom options here
+      transparent_background = true,
+      gamma = 1.00, -- adjust the brightness of the theme
+      styles = {
+          comments = { italic = true }, -- style for comments
+          keywords = { italic = true }, -- style for keywords
+          identifiers = { italic = true }, -- style for identifiers
+          functions = {}, -- style for functions
+          variables = {}, -- style for variables
+      },
+      custom_highlights = {} or function(highlights, palette) return {} end, -- extend highlights
+      custom_palette = {} or function(palette) return {} end, -- extend palette
+      terminal_colors = true, -- enable terminal colors
     },
     config = function(_, opts)
         require("tokyodark").setup(opts)
         vim.cmd [[colorscheme tokyodark]]
-    end,
+    end, 
   },
   'editorconfig/editorconfig-vim',
   {
